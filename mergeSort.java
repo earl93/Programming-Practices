5cdd6c3e7826a2a1c5262066db5878fef71beb93
Insoo Choo <earl93@hotmail.com>
2014-05-24 20:49:20 -0400

    merge sort in java

---
+++ mergeSort.java
@@ -0,0 +1,44 @@
+//Merge Sort implementation in java
+
+public static void mergeSort(int[] arr){
+	if(arr.length > 1){
+		int[] left = leftHalf(arr);
+		int[] right = rightHalf(arr);
+		mergeSort(left);
+		mergeSort(right);
+		merge(arr, left, right);
+	}
+}
+
+public static int[] leftHalf(int[] arr){
+	int size = arr.length/2;
+	int[] left = new int[size];
+	for(int i = 0; i < size; i++){
+		left[i] = arr[i];
+	}
+	return left;
+}
+
+public static int[] rightHalf(int[] arr){
+	int leftSize = arr.length/2;
+	int size = arr.length - leftSize;
+	int[] right = new int[size];
+	for(int i = 0; i < size; i++){
+		right[i] = arr[i + leftSize];
+	}
+	return right;
+}
+
+public static void merge(int[] arr, int[] left, int[] right){
+	int l = 0;
+	int r = 0;
+	for(int i = 0; i < arr.length; i++){
+		if(r >= right.length) || (l < left.length && left[l] <= right[r])){
+			arr[i] = left[l];
+			left++;
+		}else{
+			arr[i] = right[r];
+			right++;
+		}
+	}
+}
