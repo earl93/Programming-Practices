16f9bc70c235fcaa4f6ad503ffe6cd5d70ddff72
Insoo Choo <earl93@hotmail.com>
2014-05-26 20:47:03 -0400

    quickSort testing

--- quickSort.java
+++ quickSort.java
@@ -1,31 +1,40 @@
-//quick sort implementation in java
-public void quickSort(int[] arr){
-	quickSort(arr, 0, arr.length-1);
-}
-
-public void quickSort(int[] arr, int start, int end){
-	int i = start;
-	int k = end;
-	if (end - start >= 1){
-		int pivot = arr[start];
-		while (k > i){
-			while (arr[i] <= pivot && i <= end && k > i)
-				i++;
-			while (arr[k] > pivot && k >= start && k >= i)
-				k--;
-			if (k > i)
-				swap(arr, i, k);
-		}
-		swap(arr, start, k);
-		quickSort(arr, start, k-1);
-		quickSort(arr, k+1, end);
+public class quickSort{
+	public static void main(String [] args){
+		int[] arr = {1,2,5,6,7,4,3,2,8,9,1};
+		quickSort(arr);
+		for(int i=0; i<arr.length; i++)
+			System.out.println(arr[i]);
 	}
-	else
-		return;
-}
 
-public void swap(int[] arr, int a, int b){
-	int temp = arr[a];
-	arr[a] = arr[b];
-	arr[b] = temp;
+	//quick sort implementation in java
+	public static void quickSort(int[] arr){
+		quickSort(arr, 0, arr.length-1);
+	}
+
+	public static void quickSort(int[] arr, int start, int end){
+		int i = start;
+		int k = end;
+		if (end - start >= 1){
+			int pivot = arr[start];
+			while (k > i){
+				while (arr[i] <= pivot && i <= end && k > i)
+					i++;
+				while (arr[k] > pivot && k >= start && k >= i)
+					k--;
+				if (k > i)
+					swap(arr, i, k);
+			}
+			swap(arr, start, k);
+			quickSort(arr, start, k-1);
+			quickSort(arr, k+1, end);
+		}
+		else
+			return;
+	}
+
+	public static void swap(int[] arr, int a, int b){
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
 }
